86d0adba110519b66c841ccc66ee3da5aad839a7
seniorcub <farinderif@gmail.com>
2024-11-02 12:13:18 -0400

    learnt about using dotenv

--- database.js
+++ database.js
@@ -1,8 +1,19 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
-mysql.createPool({
-     host: 'localhost',
-     user: "root",
-     password: "",
-     database: "notes_app"
-})
+dotenv.config();
+
+const pool = mysql.createPool({
+     host: process.env.MYSQL_HOST,
+     user: process.env.MYSQL_USER,
+     password: process.env.MYSQL_PASSWORD,
+     database: process.env.MYSQL_DATABASE
+}).promise()
+
+async function createTable() {
+const [result] = await pool.query("SELECT * FROM notes");
+return result;
+}
+
+const notes = await createTable();
+console.log(notes);
